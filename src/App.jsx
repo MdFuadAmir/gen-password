3822f4b8d9006955c2b182ea800c212ed2423f69
Md Fuad Amir <mdfuadamir@gmail.com>
2025-12-27 01:41:08 +0600

    added project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import PasswordGenerator from "./PasswordGenerator/PasswordGenerator";
+const App = () => {
+  return (
+    <div>
+      <PasswordGenerator/>
+    </div>
+  );
+};
+
+export default App;
