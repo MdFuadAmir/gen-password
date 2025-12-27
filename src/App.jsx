38120c76a2c1225b3584eb41081f3a8fd3a7209b
Md Fuad Amir <mdfuadamir@gmail.com>
2025-12-27 16:59:49 +0600

    added password genarator page complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,183 @@
 import PasswordGenerator from "./PasswordGenerator/PasswordGenerator";
+
 const App = () => {
   return (
-    <div>
-      <PasswordGenerator/>
+    <div className="min-h-screen overflow-hidden pt-8 relative">
+      {/* Background */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co.com/XkvKMTn7/hacker-doing-online-activism-wearing-anonymous-masks-filming-video.jpg)",
+        }}
+      >
+        <div className="absolute inset-0 bg-linear-to-r from-red-400 to-red-700 opacity-15" />
+        <p className="text-green-600 opacity-50 font-mono">
+          P a s s w o r d G e n e r a t o r D e s c r i p t i o n A p a s s w o
+          r d g e n e r a t o r i s a s e c u r e t o o l t h a t h e l p s u s
+          e r s c r e a t e s t r o n g a n d r a n d o m p a s s w o r d s f o
+          r o n l i n e a c c o u n t s . I n t o d a y ’ s d i g i t a l w o r
+          l d , s e c u r i t y i s e x t r e m e l y i m p o r t a n t . W e a
+          k p a s s w o r d s c a n e a s i l y b e g u e s s e d o r h a c k e
+          d , w h i c h p u t s p e r s o n a l i n f o r m a t i o n a t r i s
+          k . A r a n d o m p a s s w o r d g e n e r a t o r s o l v e s t h i
+          s p r o b l e m b y p r o d u c i n g c o m p l e x p a s s w o r d s
+          u s i n g u p p e r c a s e l e t t e r s , l o w e r c a s e l e t t
+          e r s , n u m b e r s , a n d s p e c i a l s y m b o l s . T h i s m
+          a k e s t h e p a s s w o r d m u c h h a r d e r t o c r a c k u s i
+          n g b r u t e f o r c e o r d i c t i o n a r y a t t a c k s . A m o
+          d e r n p a s s w o r d g e n e r a t o r a l l o w s u s e r s t o c
+          u s t o m i z e t h e i r p a s s w o r d l e n g t h a n d c h o o s
+          e w h e t h e r t h e y w a n t t o i n c l u d e s y m b o l s , n u
+          m b e r s , o r c a p i t a l l e t t e r s . T h i s f l e x i b i l
+          i t y h e l p s u s e r s g e n e r a t e p a s s w o r d s t h a t m
+          e e t d i f f e r e n t s e c u r i t y r e q u i r e m e n t s . U s
+          i n g a p a s s w o r d g e n e r a t o r i s o n e o f t h e b e s t
+          w a y s t o p r o t e c t y o u r o n l i n e i d e n t i t y a n d k
+          e e p y o u r d a t a s a f e . P a s s w o r d G e n e r a t o r D e
+          s c r i p t i o n A p a s s w o r d g e n e r a t o r i s a s e c u r
+          e t o o l t h a t h e l p s u s e r s c r e a t e s t r o n g a n d r
+          a n d o m p a s s w o r d s f o r o n l i n e a c c o u n t s . I n t
+          o d a y ’ s d i g i t a l w o r l d , s e c u r i t y i s e x t r e m
+          e l y i m p o r t a n t . W e a k p a s s w o r d s c a n e a s i l y
+          b e g u e s s e d o r h a c k e d , w h i c h p u t s p e r s o n a l
+          i n f o r m a t i o n a t r i s k . A r a n d o m p a s s w o r d g e
+          n e r a t o r s o l v e s t h i s p r o b l e m b y p r o d u c i n g
+          c o m p l e x p a s s w o r d s u s i n g u p p e r c a s e l e t t e
+          r s , l o w e r c a s e l e t t e r s , n u m b e r s , a n d s p e c
+          i a l s y m b o l s . T h i s m a k e s t h e p a s s w o r d m u c h
+          h a r d e r t o c r a c k u s i n g b r u t e f o r c e o r d i c t i
+          o n a r y a t t a c k s . A m o d e r n p a s s w o r d g e n e r a t
+          o r a l l o w s u s e r s t o c u s t o m i z e t h e i r p a s s w o
+          r d l e n g t h a n d c h o o s e w h e t h e r t h e y w a n t t o i
+          n c l u d e s y m b o l s , n u m b e r s , o r c a p i t a l l e t t
+          e r s . T h i s f l e x i b i l i t y h e l p s u s e r s g e n e r a
+          t e p a s s w o r d s t h a t m e e t d i f f e r e n t s e c u r i t
+          y r e q u i r e m e n t s . U s i n g a p a s s w o r d g e n e r a t
+          o r i s o n e o f t h e b e s t w a y s t o p r o t e c t y o u r o n
+          l i n e i d e n t i t y a n d k e e p y o u r d a t a s a f e . P a s
+          s w o r d G e n e r a t o r D e s c r i p t i o n A p a s s w o r d g
+          e n e r a t o r i s a s e c u r e t o o l t h a t h e l p s u s e r s
+          c r e a t e s t r o n g a n d r a n d o m p a s s w o r d s f o r o n
+          l i n e a c c o u n t s . I n t o d a y ’ s d i g i t a l w o r l d ,
+          s e c u r i t y i s e x t r e m e l y i m p o r t a n t . W e a k p a
+          s s w o r d s c a n e a s i l y b e g u e s s e d o r h a c k e d , w
+          h i c h p u t s p e r s o n a l i n f o r m a t i o n a t r i s k . A
+          r a n d o m p a s s w o r d g e n e r a t o r s o l v e s t h i s p r
+          o b l e m b y p r o d u c i n g c o m p l e x p a s s w o r d s u s i
+          n g u p p e r c a s e l e t t e r s , l o w e r c a s e l e t t e r s
+          , n u m b e r s , a n d s p e c i a l s y m b o l s . T h i s m a k e
+          s t h e p a s s w o r d m u c h h a r d e r t o c r a c k u s i n g b
+          r u t e f o r c e o r d i c t i o n a r y a t t a c k s . A m o d e r
+          n p a s s w o r d g e n e r a t o r a l l o w s u s e r s t o c u s t
+          o m i z e t h e i r p a s s w o r d l e n g t h a n d c h o o s e w h
+          e t h e r t h e y w a n t t o i n c l u d e s y m b o l s , n u m b e
+          r s , o r c a p i t a l l e t t e r s . T h i s f l e x i b i l i t y
+          h e l p s u s e r s g e n e r a t e p a s s w o r d s t h a t m e e t
+          d i f f e r e n t s e c u r i t y r e q u i r e m e n t s . U s i n g
+          a p a s s w o r d g e n e r a t o r i s o n e o f t h e b e s t w a y
+          s t o p r o t e c t y o u r o n l i n e i d e n t i t y a n d k e e p
+          y o u r d a t a s a f e . P a s s w o r d G e n e r a t o r D e s c r
+          i p t i o n A p a s s w o r d g e n e r a t o r i s a s e c u r e t o
+          o l t h a t h e l p s u s e r s c r e a t e s t r o n g a n d r a n d
+          o m p a s s w o r d s f o r o n l i n e a c c o u n t s . I n t o d a
+          y ’ s d i g i t a l w o r l d , s e c u r i t y i s e x t r e m e l y
+          i m p o r t a n t . W e a k p a s s w o r d s c a n e a s i l y b e g
+          u e s s e d o r h a c k e d , w h i c h p u t s p e r s o n a l i n f
+          o r m a t i o n a t r i s k . A r a n d o m p a s s w o r d g e n e r
+          a t o r s o l v e s t h i s p r o b l e m b y p r o d u c i n g c o m
+          p l e x p a s s w o r d s u s i n g u p p e r c a s e l e t t e r s ,
+          l o w e r c a s e l e t t e r s , n u m b e r s , a n d s p e c i a l
+          s y m b o l s . T h i s m a k e s t h e p a s s w o r d m u c h h a r
+          d e r t o c r a c k u s i n g b r u t e f o r c e o r d i c t i o n a
+          r y a t t a c k s . A m o d e r n p a s s w o r d g e n e r a t o r a
+          l l o w s u s e r s t o c u s t o m i z e t h e i r p a s s w o r d l
+          e n g t h a n d c h o o s e w h e t h e r t h e y w a n t t o i n c l
+          u d e s y m b o l s , n u m b e r s , o r c a p i t a l l e t t e r s
+          . T h i s f l e x i b i l i t y h e l p s u s e r s g e n e r a t e p
+          a s s w o r d s t h a t m e e t d i f f e r e n t s e c u r i t y r e
+          q u i r e m e n t s . U s i n g a p a s s w o r d g e n e r a t o r i
+          s o n e o f t h e b e s t w a y s t o p r o t e c t y o u r o n l i n
+          e i d e n t i t y a n d k e e p y o u r d a t a s a f e . P a s s w o
+          r d G e n e r a t o r D e s c r i p t i o n A p a s s w o r d g e n e
+          r a t o r i s a s e c u r e t o o l t h a t h e l p s u s e r s c r e
+          a t e s t r o n g a n d r a n d o m p a s s w o r d s f o r o n l i n
+          e a c c o u n t s . I n t o d a y ’ s d i g i t a l w o r l d , s e c
+          u r i t y i s e x t r e m e l y i m p o r t a n t . W e a k p a s s w
+          o r d s c a n e a s i l y b e g u e s s e d o r h a c k e d , w h i c
+          h p u t s p e r s o n a l i n f o r m a t i o n a t r i s k . A r a n
+          d o m p a s s w o r d g e n e r a t o r s o l v e s t h i s p r o b l
+          e m b y p r o d u c i n g c o m p l e x p a s s w o r d s u s i n g u
+          p p e r c a s e l e t t e r s , l o w e r c a s e l e t t e r s , n u
+          m b e r s , a n d s p e c i a l s y m b o l s . T h i s m a k e s t h
+          e p a s s w o r d m u c h h a r d e r t o c r a c k u s i n g b r u t
+          e f o r c e o r d i c t i o n a r y a t t a c k s . A m o d e r n p a
+          s s w o r d g e n e r a t o r a l l o w s u s e r s t o c u s t o m i
+          z e t h e i r p a s s w o r d l e n g t h a n d c h o o s e w h e t h
+          e r t h e y w a n t t o i n c l u d e s y m b o l s , n u m b e r s ,
+          o r c a p i t a l l e t t e r s . T h i s f l e x i b i l i t y h e l
+          p s u s e r s g e n e r a t e p a s s w o r d s t h a t m e e t d i f
+          f e r e n t s e c u r i t y r e q u i r e m e n t s . U s i n g a p a
+          s s w o r d g e n e r a t o r i s o n e o f t h e b e s t w a y s t o
+          p r o t e c t y o u r o n l i n e i d e n t i t y a n d k e e p y o u
+          r d a t a s a f e . P a s s w o r d G e n e r a t o r D e s c r i p t
+          i o n A p a s s w o r d g e n e r a t o r i s a s e c u r e t o o l t
+          h a t h e l p s u s e r s c r e a t e s t r o n g a n d r a n d o m p
+          a s s w o r d s f o r o n l i n e a c c o u n t s . I n t o d a y ’ s
+          d i g i t a l w o r l d , s e c u r i t y i s e x t r e m e l y i m p
+          o r t a n t . W e a k p a s s w o r d s c a n e a s i l y b e g u e s
+          s e d o r h a c k e d , w h i c h p u t s p e r s o n a l i n f o r m
+          a t i o n a t r i s k . A r a n d o m p a s s w o r d g e n e r a t o
+          r s o l v e s t h i s p r o b l e m b y p r o d u c i n g c o m p l e
+          x p a s s w o r d s u s i n g u p p e r c a s e l e t t e r s , l o w
+          e r c a s e l e t t e r s , n u m b e r s , a n d s p e c i a l s y m
+          b o l s . T h i s m a k e s t h e p a s s w o r d m u c h h a r d e r
+          t o c r a c k u s i n g b r u t e f o r c e o r d i c t i o n a r y a
+          t t a c k s . A m o d e r n p a s s w o r d g e n e r a t o r a l l o
+          w s u s e r s t o c u s t o m i z e t h e i r p a s s w o r d l e n g
+          t h a n d c h o o s e w h e t h e r t h e y w a n t t o i n c l u d e
+          s y m b o l s , n u m b e r s , o r c a p i t a l l e t t e r s . T h
+          i s f l e x i b i l i t y h e l p s u s e r s g e n e r a t e p a s s
+          w o r d s t h a t m e e t d i f f e r e n t s e c u r i t y r e q u i
+          r e m e n t s . U s i n g a p a s s w o r d g e n e r a t o r i s o n
+          e o f t h e b e s t w a y s t o p r o t e c t y o u r o n l i n e i d
+          e n t i t y a n d k e e p y o u r d a t a s a f e . P a s s w o r d G
+          e n e r a t o r D e s c r i p t i o n A p a s s w o r d g e n e r a t
+          o r i s a s e c u r e t o o l t h a t h e l p s u s e r s c r e a t e
+          s t r o n g a n d r a n d o m p a s s w o r d s f o r o n l i n e a c
+          c o u n t s . I n t o d a y ’ s d i g i t a l w o r l d , s e c u r i
+          t y i s e x t r e m e l y i m p o r t a n t . W e a k p a s s w o r d
+          s c a n e a s i l y b e g u e s s e d o r h a c k e d , w h i c h p u
+          t s p e r s o n a l i n f o r m a t i o n a t r i s k . A r a n d o m
+          p a s s w o r d g e n e r a t o r s o l v e s t h i s p r o b l e m b
+          y p r o d u c i n g c o m p l e x p a s s w o r d s u s i n g u p p e
+          r c a s e l e t t e r s , l o w e r c a s e l e t t e r s , n u m b e
+          r s , a n d s p e c i a l s y m b o l s . T h i s m a k e s t h e p a
+          s s w o r d m u c h h a r d e r t o c r a c k u s i n g b r u t e f o
+          r c e o r d i c t i o n a r y a t t a c k s . A m o d e r n p a s s w
+          o r d g e n e r a t o r a l l o w s u s e r s t o c u s t o m i z e t
+          h e i r p a s s w o r d l e n g t h a n d c h o o s e w h e t h e r t
+          h e y w a n t t o i n c l u d e s y m b o l s , n u m b e r s , o r c
+          a p i t a l l e t t e r s . T h i s f l e x i b i l i t y h e l p s u
+          s e r s g e n e r a t e p a s s w o r d s t h a t m e e t d i f f e r
+          e n t s e c u r i t y r e q u i r e m e n t s . U s i n g a p a s s w
+          o r d g e n e r a t o r i s o n e o f t h e b e s t w a y s t o p r o
+          t e c t y o u r o n l i n e i d e n t i t y a n d k e e p y o u r d a
+          t a s a f e .
+        </p>
+      </div>
+      <div className="relative pb-8 px-4">
+        <PasswordGenerator />
+      </div>
+      <div className="relative">
+        <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+          <aside>
+            <p className="text-white text-center">
+              Copyright © {new Date().getFullYear()} - Md Fuad Amir All right reserved
+            </p>
+          </aside>
+        </footer>
+      </div>
     </div>
   );
 };
--- src/PasswordGenerator/PasswordGenerator.jsx
+++ src/PasswordGenerator/PasswordGenerator.jsx
@@ -1,11 +1,147 @@
-import React from 'react';
-
+import { useState } from "react";
+import { FaCopy, FaLock } from "react-icons/fa";
+import logo from "../assets/pass.png";
 const PasswordGenerator = () => {
-    return (
-        <div>
-            
+  const [length, setLength] = useState(12);
+  const [password, setPassword] = useState("");
+  const [options, setOptions] = useState({
+    uppercase: true,
+    lowercase: true,
+    numbers: true,
+    symbols: false,
+  });
+
+  const generatePassword = () => {
+    let chars = "";
+    if (options.uppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (options.lowercase) chars += "abcdefghijklmnopqrstuvwxyz";
+    if (options.numbers) chars += "0123456789";
+    if (options.symbols) chars += "!@#$%^&*()_+[]{}<>?";
+
+    if (!chars) {
+      alert("Select at least one option!");
+      return;
+    }
+
+    let pass = "";
+    for (let i = 0; i < length; i++) {
+      pass += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    setPassword(pass);
+  };
+
+  const copyPassword = () => {
+    if (!password) return;
+    navigator.clipboard.writeText(password);
+    alert("Password copied!");
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div
+        className="
+          w-full max-w-md
+          bg-white/10 backdrop-blur-xl
+          border border-white/20
+          rounded-2xl
+          shadow-2xl
+          p-6
+          text-white
+        "
+      >
+        <img src={logo} alt="" className="w-44 h-44 mx-auto" />
+        {/* Title */}
+        <h1 className="text-2xl font-bold text-center mb-6 flex items-center justify-center gap-2">
+          <FaLock className="text-red-400" />
+          Password Generator
+        </h1>
+        {/* Password Display */}
+        <div className="flex items-center mb-5 rounded-xl overflow-hidden border border-white/20 bg-black/30">
+          <input
+            type="text"
+            value={password}
+            readOnly
+            placeholder="Generated password"
+            className="
+              flex-1 px-4 py-3 bg-transparent
+              text-white placeholder-gray-400
+              outline-none
+            "
+          />
+          <button
+            onClick={copyPassword}
+            className="
+              px-4 py-3
+              text-gray-300 hover:text-red-400
+              transition
+            "
+          >
+            <FaCopy />
+          </button>
         </div>
-    );
+        {/* Length */}
+        <div className="mb-5">
+          <label className="flex justify-between text-sm mb-2">
+            <span>Password Length</span>
+            <span className="font-semibold text-red-400">{length}</span>
+          </label>
+          <input
+            type="range"
+            min="6"
+            max="30"
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+            className="w-full accent-red-500"
+          />
+        </div>
+        {/* Options */}
+        <div className="space-y-3 mb-6 text-sm">
+          {[
+            { key: "uppercase", label: "Include Uppercase Letters" },
+            { key: "lowercase", label: "Include Lowercase Letters" },
+            { key: "numbers", label: "Include Numbers" },
+            { key: "symbols", label: "Include Symbols" },
+          ].map((opt) => (
+            <label
+              key={opt.key}
+              className="
+                flex items-center justify-between
+                bg-white/5
+                px-4 py-2
+                rounded-lg
+                border border-white/10
+              "
+            >
+              <span>{opt.label}</span>
+              <input
+                type="checkbox"
+                checked={options[opt.key]}
+                onChange={() =>
+                  setOptions({ ...options, [opt.key]: !options[opt.key] })
+                }
+                className="accent-red-500 w-4 h-4"
+              />
+            </label>
+          ))}
+        </div>
+        {/* Button */}
+        <button
+          onClick={generatePassword}
+          className="
+            w-full py-3 rounded-xl
+            bg-linear-to-r from-red-500 to-red-700
+            hover:from-red-600 hover:to-red-800
+            font-semibold
+            tracking-wide
+            shadow-lg
+            transition
+          "
+        >
+          Generate Password
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PasswordGenerator;
